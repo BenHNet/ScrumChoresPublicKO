65aed595272fdf6da00ddffc49232f98f0a3ba34
Darth Hoelting <ben@htechinc.com>
2015-03-01 21:44:06 -0700

    Added bootstrap theme

--- ScrumChoresPublicKO/App_Start/BundleConfig.cs
+++ ScrumChoresPublicKO/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace ScrumChoresPublicKO
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/font-awesome.css",
+                      "~/Content/style.css",
+                      "~/Content/blue.css"));
         }
     }
 }
