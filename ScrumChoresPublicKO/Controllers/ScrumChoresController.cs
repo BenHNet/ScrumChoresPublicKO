103cbe001f8781d5ea32410416abc0c56b6ecf4e
Darth Hoelting <ben@htechinc.com>
2015-03-07 10:05:13 -0700

    Got navigation working
    Got the My Scrum Chores Working

---
+++ ScrumChoresPublicKO/Controllers/ScrumChoresController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumChoresPublicKO.Controllers
+{
+    public class ScrumChoresController : Controller
+    {
+        // GET: ScrumChores
+        public ActionResult Home()
+        {
+            return View();
+        }
+        // GET: ChoreList
+        public ActionResult ChoreList()
+        {
+            return View();
+        }
+        // GET: ChoreBoard
+        public ActionResult ChoreBoard()
+        {
+            return View();
+        }
+    }
+}
